3e155e07ac6ba54e0fd5cab410ae6a6330d70cb3
KawaiiFoox08 <96639787+Foox-dev@users.noreply.github.com>
2022-11-30 19:19:20 -0400

    Fixed stuff

--- Stormtides_data/bin/core.bat
+++ Stormtides_data/bin/core.bat
@@ -1,41 +1,4 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWGLUohhgeUEFFESyEVb6b5YO7env5uSA4mglcYI=
-::fBE1pAF6MU+EWGLUohhgeUEFFESyEVb6b5MV5O317ueC+A0+Dt4Ka4rJyYiPLO8B43nAepU4239UjNgwPy96PjezazMHvWFQu3CMM8KPjAvgXkm15EI+J2Z7lWaDwi4jZbM=
-::fBE1pAF6MU+EWGLUohhgeUEFFESyEVb6b5MT+uX6+7DH8R9ddusrOJOb+buAM+8f7wWsQ58+33hWnYJs
-::fBE1pAF6MU+EWGLUohhgeUEFFESyEVb6b5QY7OH16KqVp14SQfA8fZyVlPrOD8tz
-::fBE1pAF6MU+EWGLUohhgeUEFFESyEVb6b4UO5+v+/PnHpEQTXfE3fYu7
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdCyDJGyX8VAjFBpYQxaLAE+1EbsQ5+n//Na0tkIPWdY2fIrIl5iBJ+9TohStO58u2Ro=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBpYQxaLAE+1BaAR7ebv/Na0lm9dcfo6RLzP1aaDNOkX71bbcZc510ZWkckwDxRKe0HlaxcxyQ==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+
 @echo off
 mode con cols=48 lines=35
 setlocal enabledelayedexpansion
